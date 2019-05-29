61aea447e9ca859e37227badf394c544a4df533a
Mary <marykwong27@gmail.com>
2019-05-28 20:07:28 -0700

    update

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+var friends = [{
+    "name": "Monica",
+    "photo": "https://jenniferdukeslee.com/wp-content/uploads/2018/08/monica-geller.jpg",
+    "scores": [1, 1, 4, 3, 5, 1, 5, 5, 4, 4]
+},
+{
+    "name": "Joey",
+    "photo": "https://data.whicdn.com/images/315939383/large.jpg",
+    "scores": [3, 2, 1, 1, 5, 2, 2, 2, 3, 2]
+},
+{
+    "name": "Chandler",
+    "photo": "https://i.ytimg.com/vi/bbCpnwlSJMM/hqdefault.jpg",
+    "scores": [5, 1, 2, 5, 4, 1, 3, 2, 1, 5]
+},
+{
+    "name": "Phoebe",
+    "photo": "https://i.pinimg.com/originals/5a/ee/d9/5aeed9bed09478de1a106480200b31d3.jpg",
+    "scores": [3, 2, 4, 2, 5, 1, 3, 2, 1, 3]
+},
+{
+    "name": "Rachel",
+    "photo": "http://25.media.tumblr.com/tumblr_m7ip551r3y1r3ey0mo1_500.jpg",
+    "scores": [4, 3, 5, 1, 5, 1, 5, 2, 1, 4]
+},
+{
+    "name": "Ross",
+    "photo": "https://ksassets.timeincuk.net/wp/uploads/sites/55/2019/01/ross-gellernew-1-920x584.jpg",
+    "scores": [3, 5, 4, 1, 2, 3, 1, 3, 5, 4]
+},
+{
+    "name": "Gunther",
+    "photo": "https://i.ytimg.com/vi/lzs6Wm0xUK4/maxresdefault.jpg",
+    "scores": [2, 5, 2, 3, 1, 2, 4, 2, 4, 5]
+}
+
+]
+
+module.exports = friends;
